d6523f8a4c7e62a1b680a9ef7fdfd65ef24063ec
Björn Dahlstrand <bjorndahlstrand@gmail.com>
2017-10-10 15:12:25 +0200

    test2

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+//var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+//var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ipaddress   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
 
 var WebSocketServer = require('ws').Server;
 var url = require('url');
